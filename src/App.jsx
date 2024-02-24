6270f3f2732478855f25c7c9bf46a1617135a709
sarrabs22 <sarra.bensedrine@sprit.tn>
2024-02-24 18:12:41 +0100

    crud_event&Cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,27 @@
-import { useState } from 'react'
-import Home from './ClientComponent/home'
-import { Routes, Route } from 'react-router-dom'
-import Admin from './AdminComponent/Admin'
-import Candidates from './ClientComponent/Models/Candidat'
-import CandidatsP from './ClientComponent/CandidatsP'
-
-
+import { useState } from 'react';
+import Home from './ClientComponent/home';
+import { Routes, Route } from 'react-router-dom';
+import Admin from './AdminComponent/Admin';
+import Candidates from './ClientComponent/Models/Candidat';
+import CandidatsP from './ClientComponent/CandidatsP';
+import Evenement from './ClientComponent/Evenement';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0); // Si vous n'utilisez pas 'count' et 'setCount', vous pouvez les retirer.
 
   return (
-    <>
-      <div>
-
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/CandidatsP" element={<CandidatsP />} />
-            <Route path="/candidat" element={<Candidates />} />
-
-
-            <Route path="/admin/*" element={<Admin />} />
-
-
-          </Routes>
-
-
-          <Route path="/candidat" element={<Candidates />} />
-          <Route path="/admin/*" element={<Admin />} />    {/* <Route path="/candidats" element={<Candidats />} /> */}
-
-
-
-        </Routes>
-
-      </div>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/CandidatsP" element={<CandidatsP />} />
+        <Route path="/candidat" element={<Candidates />} />
+        <Route path="/evenements" element={<Evenement />} />
+        <Route path="/admin/*" element={<Admin />} />
+        {/* Supprimez les lignes commentées ou en double ci-dessous */}
+        {/* <Route path="/candidats" element={<Candidats />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
