0d418c824963c88decd052282fc7a717b8d96580
sarrabs22 <sarra.bensedrine@sprit.tn>
2024-02-27 14:50:12 +0100

    changes

--- src/ClientComponent/Dashboard/HeaderClient.jsx
+++ src/ClientComponent/Dashboard/HeaderClient.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 function HeaderClient() {
-  return (
-    <div> <header className="header-area header-sticky">
-    <div className="container">
-        <div className="row">
-            <div className="col-12">
-                <nav className="main-nav"><Link to="/"> 
-                    <a className="logo">
-                        Talent<span className="text-danger">Hub</span>
-                    </a>
-                       </Link>
-                    <ul className="nav">
-                        <li className="scroll-to-section"><Link to="/">Home</Link></li>                             
-                        <li className="scroll-to-section"><Link to="/offers">Opprotunités D'emploi</Link></li>
-                        <li className="scroll-to-section"><Link to="/CandidatsP">Postulez Maintenant</Link></li>  
-                        <li className="has-sub">
-                            <a href="javascript:void(0)">Pages</a>
-                            <ul className="sub-menu">
-                                <li><a >Offres</a></li>
-                                <li><a >Detail Des Offre</a></li>
+    return (
+        <div> <header className="header-area header-sticky">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <nav className="main-nav"><Link to="/">
+                            <a className="logo">
+                                Talent<span className="text-danger">Hub</span>
+                            </a>
+                        </Link>
+                            <ul className="nav">
+                                <li className="scroll-to-section"><Link to="/">Home</Link></li>
+                                <li className="scroll-to-section"><Link to="/offers">Opprotunités D'emploi</Link></li>
+                                <li className="scroll-to-section"><Link to="/CandidatsP">Postulez Maintenant</Link></li>
+                                <li className="has-sub">
+                                    <a href="javascript:void(0)">Pages</a>
+                                    <ul className="sub-menu">
+                                        <li>< Link to="/evenements">Evenements</Link></li>
+                                        <li><a >Detail Des Offre</a></li>
+                                    </ul>
+                                </li>
+
+                                <li className="scroll-to-section"><a href="#contact">Contactez-nous</a></li>
                             </ul>
-                        </li>
-                        
-                        <li className="scroll-to-section"><a href="#contact">Contactez-nous</a></li> 
-                    </ul>        
-                    <a className='menu-trigger'>
-                        <span>Menu</span>
-                    </a>
-                    
-                </nav>
+                            <a className='menu-trigger'>
+                                <span>Menu</span>
+                            </a>
+
+                        </nav>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</header></div>
-  )
+        </header></div>
+    )
 }
 
 export default HeaderClient
