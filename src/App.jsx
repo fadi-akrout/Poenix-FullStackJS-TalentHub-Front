3aa5bb463c6fec5c4b87be86020eb96f03b4d800
fvd10 <80286835+fadi-akrout@users.noreply.github.com>
2024-03-01 16:29:04 +0100

    Offer update & delete node modules conflit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React, { useState, Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import CandidateForm from './ClientComponent/CandidatsComponent/AddCandidate';
 
 const Home = lazy(() => import('./ClientComponent/Dashboard/home'));
 const Admin = lazy(() => import('./AdminComponent/Admin'));
-const Candidates = lazy(() => import('./ClientComponent/Models/Candidat'));
-const AddCandidats = lazy(() => import('./ClientComponent/CandidatsComponent/AddCandidats'));
+const Candidate = lazy(() => import('./ClientComponent/CandidatsComponent/AddCandidate'));
 const Evenement = lazy(() => import('./ClientComponent/EventComponent/Evenement'));
 const AddEvent = lazy(() => import('./ClientComponent/EventComponent/AddEvent'));
 const AddOffer = lazy(() => import('./ClientComponent/OfferComponent/AddOffer'));
 const OfferList = lazy(() => import('./ClientComponent/OfferComponent/OfferList'));
+const Login = lazy(() => import('./ClientComponent/UserComponent/Login'));
+const Signup = lazy(() => import('./ClientComponent/UserComponent/Signup'));
 const UpdateOffer = lazy(() => import('./ClientComponent/OfferComponent/UpdateOffer'));
+
+
 
 
 function App() {
@@ -21,14 +24,19 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/AddCandidats" element={<AddCandidats />} />
-          <Route path="/candidat" element={<Candidates />} />
+          <Route path="/AddCandidate" element={<Candidate />} />
           <Route path="/evenements" element={<Evenement />} />
           <Route path="/add-event" element={<AddEvent />} />
           <Route path="/admin/*" element={<Admin />} />
           <Route path="/offers" element={<OfferList />} />
+
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+
+
           <Route path="/addoffer" element={<AddOffer />} />
           <Route path="/updateoffer/:id" element={<UpdateOffer />} />
+
 
         </Routes>
       </Suspense>
