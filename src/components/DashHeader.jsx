e08be343f657120ec08186bd8c3c255cbf4d9771
laroussiamir01 <amir.laroussi@esprit.tn>
2024-03-21 18:37:30 +0100

    roleBasedAccess

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,15 +1,22 @@
 import { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import { faFileCirclePlus,
+    faFilePen,
+    faUserGear,
+    faUserPlus,
+    faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
 import { useNavigate, Link, useLocation } from 'react-router-dom'
 
 import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+
+import useAuth from '../hooks/useAuth'
 
 const DASH_REGEX = /^\/dash(\/)?$/
 const NOTES_REGEX = /^\/dash\/notes(\/)?$/
 const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
+    const { isStudent, isAdmin } = useAuth()
 
     const navigate = useNavigate()
     const { pathname } = useLocation()
@@ -25,13 +32,70 @@ const DashHeader = () => {
         if (isSuccess) navigate('/')
     }, [isSuccess, navigate])
 
-    if (isLoading) return <p>Logging Out...</p>
+    const onNewNoteClicked = () => navigate('/dash/notes/new')
+    const onNewUserClicked = () => navigate('/dash/users/new')
+    const onNotesClicked = () => navigate('/dash/notes')
+    const onUsersClicked = () => navigate('/dash/users')
 
-    if (isError) return <p>Error: {error.data?.message}</p>
+  
 
     let dashClass = null
     if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
         dashClass = "dash-header__container--small"
+    }
+
+    let newNoteButton = null
+    if (NOTES_REGEX.test(pathname)) {
+        newNoteButton = (
+            <button
+                className="icon-button"
+                title="New Note"
+                onClick={onNewNoteClicked}
+            >
+                <FontAwesomeIcon icon={faFileCirclePlus} />
+            </button>
+        )
+    }
+
+    let newUserButton = null
+    if (USERS_REGEX.test(pathname)) {
+        newUserButton = (
+            <button
+                className="icon-button"
+                title="New User"
+                onClick={onNewUserClicked}
+            >
+                <FontAwesomeIcon icon={faUserPlus} />
+            </button>
+        )
+    }
+
+    let userButton = null
+    if ( isAdmin) {
+        if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
+            userButton = (
+                <button
+                    className="icon-button"
+                    title="Users"
+                    onClick={onUsersClicked}
+                >
+                    <FontAwesomeIcon icon={faUserGear} />
+                </button>
+            )
+        }
+    }
+
+    let notesButton = null
+    if (!NOTES_REGEX.test(pathname) && pathname.includes('/dash')) {
+        notesButton = (
+            <button
+                className="icon-button"
+                title="Notes"
+                onClick={onNotesClicked}
+            >
+                <FontAwesomeIcon icon={faFilePen} />
+            </button>
+        )
     }
 
     const logoutButton = (
@@ -44,7 +108,27 @@ const DashHeader = () => {
         </button>
     )
 
+    const errClass = isError ? "errmsg" : "offscreen"
+    let buttonContent
+    if (isLoading) {
+        buttonContent = <p>Logging Out...</p>
+    } else {
+        buttonContent = (
+            <>
+                {newNoteButton}
+                {newUserButton}
+                {notesButton}
+                {userButton}
+                {logoutButton}
+            </>
+        )
+    }
+
+
     const content = (
+        <>
+        <p className={errClass}>{error?.data?.message}</p>
+
         <header className="dash-header">
             <div className={`dash-header__container ${dashClass}`}>
                 <Link to="/dash">
@@ -52,10 +136,11 @@ const DashHeader = () => {
                 </Link>
                 <nav className="dash-header__nav">
                     {/* add more buttons later */}
-                    {logoutButton}
-                </nav>
+                    {buttonContent}         
+                           </nav>
             </div>
         </header>
+        </>
     )
 
     return content
--- src/features/notes/EditNote.jsx
+++ src/features/notes/EditNote.jsx
@@ -5,7 +5,7 @@ import { selectAllUsers } from '../users/usersApiSlice'
 import EditNoteForm from './EditNoteForm'
 
 const EditNote = () => {
-     const { id } = useParams()
+    const { id } = useParams()
 
     const note = useSelector(state => selectNoteById(state, id))
     const users = useSelector(selectAllUsers)
